1b1f7d0e668cf7283398cb303e772172ad8ebb17
pThang <pthang301@gmail.com>
2017-12-08 08:13:24 +0700

    TestForRegister

--- PPCRental.AcceptanceTests/UC_006_RegisterSteps.cs
+++ PPCRental.AcceptanceTests/UC_006_RegisterSteps.cs
@@ -3,7 +3,6 @@ using TechTalk.SpecFlow;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
-
 namespace PPCRental.AcceptanceTests
 {
     [Binding]
@@ -14,16 +13,16 @@ namespace PPCRental.AcceptanceTests
         public void GivenIMInHomePage()
         {
             driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://ppcrental.tk/Home/Index");
+            driver.Navigate().GoToUrl("http://localhost:53887/");
         }
-
+        
         [Given(@"I choose Register button")]
         public void GivenIChooseRegisterButton()
         {
             driver.FindElement(By.Id("register")).Click();
             ScenarioContext.Current.Pending();
         }
-
+        
         [When(@"I input my information and click Submit")]
         public void WhenIInputMyInformationAndClickSubmit()
         {
@@ -33,51 +32,14 @@ namespace PPCRental.AcceptanceTests
             driver.FindElement(By.Id("ConfirmPassword")).SendKeys("123456");
             driver.FindElement(By.Id("Address")).SendKeys("20 Nguyen Khac Nhu");
             driver.FindElement(By.Id("Phone")).SendKeys("0982817691");
+            driver.FindElement(By.Id("submit")).Click();
+            ScenarioContext.Current.Pending();
+        }
 
+        [Then(@"the result is successfull and send you back to HomePage")]
+        public void ThenTheResultIsSuccessfullAndSendYouBackToHomePage()
+        {
+            ScenarioContext.Current.Pending();
         }
     }
 }
-//namespace PPCRental.AcceptanceTests
-//    {
-//        [Binding]
-//        public class UC_006_RegisterSteps
-//        {
-//            public IWebDriver driver;
-//            [Given(@"I'm in HomePage")]
-//            public void GivenIMInHomePage()
-//            {
-//                driver = new ChromeDriver();
-//                driver.Navigate().GoToUrl("https://ppcrental.tk/Home/Index");
-//            }
-
-//            [Given(@"I choose Register button")]
-//            public void GivenIChooseRegisterButton()
-//            {
-
-//                driver.FindElement(By.Id("register")).Click();
-//            }
-
-//            [When(@"I input my information and click Submit")]
-//            public void WhenIInputMyInformationAndClickSubmit()
-//            {
-//                driver.FindElement(By.Id("Username")).SendKeys("pThang");
-//                driver.FindElement(By.Id("Email")).SendKeys("Thang@gmail.com");
-//                driver.FindElement(By.Id("password")).SendKeys("123456");
-//                driver.FindElement(By.Id("confirm")).SendKeys("123456");
-//                driver.FindElement(By.Id("address")).SendKeys("20 Nguyen Khach Nhu");
-//                driver.FindElement(By.Id("phone:")).SendKeys("0908000000");
-//                driver.FindElement(By.Id("submit")).Click();
-//                ScenarioContext.Current.Pending();
-//            }
-
-//            [Then(@"the result is successfull and send you back to HomePage")]
-//            public void ThenTheResultIsAndSendYouBackToHomePage(string p0)
-//            {
-//                ScenarioContext.Current.Pending();
-//            }
-//        }
-//    }
-
-//}
-
-
