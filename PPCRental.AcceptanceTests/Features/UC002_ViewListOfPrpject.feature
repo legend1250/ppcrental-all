b0af1067c327d284e4ab7965703cc111603672f5
Vinh <vinh_nguyen1211@yahoo.com.vn>
2017-12-13 15:59:08 +0700

    update testing for View List of Project

---
+++ PPCRental.AcceptanceTests/Features/UC002_ViewListOfPrpject.feature
@@ -0,0 +1,8 @@
+﻿Feature: UC002_ViewListOfProject
+	
+
+@mytag
+Scenario: View list pf project success
+	Given I am in homepage
+	When I press property button
+	Then I see list of project
---
+++ PPCRental.AcceptanceTests/StepDefinitions/ViewListOfPrpjectSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PPCRental.AcceptanceTests.StepDefinitions
+{
+    [Binding]
+    public class UC002_ViewListOfPrpjectSteps
+    {
+        IWebDriver driver;
+        [Given(@"I am in homepage")]
+        public void GivenIAmInHomepage()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://localhost:53887/");
+        }
+        
+        [When(@"I press property button")]
+        public void WhenIPressPropertyButton()
+        {
+            driver.FindElement(By.XPath("//*[@id='property']")).Click();
+        }
+        
+        [Then(@"I see list of project")]
+        public void ThenISeeListOfProject()
+        {
+            string element = driver.FindElement(By.CssSelector("#property-listing > div > div > div:nth-child(1) > article > div > header > div > h6 > a")).Text;
+            Assert.AreEqual("ASA LightT", element);
+        }
+    }
+}
